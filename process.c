dd2e2c07bac0903765b24a85347b7c450a35adc4
Jan Hranička <hranickajan@seznam.cz>
2022-04-30 15:53:42 +0200

    Test of arg_to_int implementation

--- process.c
+++ process.c
@@ -7,6 +7,16 @@
 
 int main(int argc, char* argv[]) {
     
+    if(argc != 5) {
+        fprintf(stderr, "process.c: Not enough arguments\n");
+        exit(1);
+    }
 
+    if(arg_to_int(argv[1], &NO)) {
+        fprintf(stderr, "process.c: Argument must be a natural number\n");
+        exit(1);
+    }
+
+    printf("%d\n", NO);
     
 }
